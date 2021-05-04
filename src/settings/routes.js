49a0fbdf5293d7ce1f3f7bff710d60f99a60f769
k073 <vlapteb@nurtelecom.kg>
2021-05-04 13:26:04 +0600

    refactor routes

---
+++ src/settings/routes.js
@@ -0,0 +1,8 @@
+const routes = {
+    list: '/',
+    login: '/login',
+    create: '/create',
+    edit: (id) => `/edit/${id}`
+}
+
+export default routes;
--- src/store/sagas/userSagas.js
+++ src/store/sagas/userSagas.js
@@ -2,11 +2,12 @@ import {put, takeEvery} from 'redux-saga/effects';
 import axios from '../../settings/axios-api'
 import formDataBuilder from "../../utils/formDataBuilder";
 import {LOGIN_USER_REQUEST, loginUserFailure, loginUserSuccess} from "../actions/userActions";
+import routes from "../../settings/routes";
 
 function* loginUser({authData}) {
     try {
         const formData = formDataBuilder(authData);
-        const response = yield axios.post('/login', formData);
+        const response = yield axios.post(routes.login, formData);
         yield put(loginUserSuccess(response.data))
     } catch (e) {
         yield put(loginUserFailure(e.data))
